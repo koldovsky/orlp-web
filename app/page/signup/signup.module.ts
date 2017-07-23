bcd3728c15941214cfc3ad5224c5e244d662da66
Vlad <vkiro@yandex.ua>
2017-07-23 17:16:01 +0300

    Refactored for push.

--- app/page/signup/signup.module.ts
+++ app/page/signup/signup.module.ts
@@ -1,29 +1,33 @@
 import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
 import {CommonModule} from "@angular/common";
-import {SignUpComponent} from "./signup.component";
 import {RouterModule} from "@angular/router";
+import {HttpModule} from "@angular/http";
+import {SignUpComponent} from "./signup.component";
 import {SignupService} from "./signup.service";
 import {AccountVerificationComponent} from "./accountVerification/accountVerification.component";
-import {AccountVerificationService} from "./accountVerification/accountVerification.service";
+
 
 @NgModule({
     imports: [
+        HttpModule,
         FormsModule,
         ReactiveFormsModule,
         CommonModule,
         RouterModule.forRoot([
             {path: 'registr', component: SignUpComponent},
-            {path: 'registrationConfirm', component: AccountVerificationComponent}
-        ])
+            {path: 'registrationConfirm', component: AccountVerificationComponent},
+
+        ]),
     ],
     exports: [],
     declarations: [
-        SignUpComponent, AccountVerificationComponent
-    ],
-    providers: [
-        SignupService, AccountVerificationService
-    ],
+        SignUpComponent,AccountVerificationComponent],
+
+    providers: [SignupService],
 })
 export class SignUpModule {
 }
