f5e232b3f5b8242a90c923861409fe04b7a660da
Vlad <vkiro@yandex.ua>
2017-07-23 16:46:40 +0300

    Changed courses DTO. Added link for decks.

--- app/classes/dto.Converter.ts
+++ app/classes/dto.Converter.ts
@@ -7,13 +7,15 @@ import {link} from "fs";
 import {CategoriesPublic} from "./CategoryDTO/public.categories";
 import {CourseTop} from "./CourseDTO/top.course.DTO";
 import {UsersPublic} from "./public.users.DTO";
+import {CourseLink} from "./CourseDTO/link.course.DTO";
 
 export class DTOConverter {
 
-    public static jsonToPublicCourse(data: any): CoursePublic {
+    public static jsonToPublicCourse(data: any): CourseLink {
         let self: Link = DTOConverter.jsonToLink("self", data._links.self);
+        let decks: Link = DTOConverter.jsonToLink("decks", data._links.decks);
 
-        return new CoursePublic(data.name, data.description, data.imagebase64, self);
+        return new CourseLink(data.name, data.description, data.imagebase64, self, decks);
     }
 
     public static jsonToPublicDeck(data: any): DeckPublic {
