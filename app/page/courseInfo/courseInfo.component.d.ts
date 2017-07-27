8202c2921a0d33a2dd87d504e1834675b98a0a27
zadorovskyi <zadorovskyi@gmail.com>
2017-07-27 11:10:23 +0300

    added top courses (#4)
    * added top courses
    
    * changed spacing

---
+++ app/page/courseInfo/courseInfo.component.d.ts
@@ -0,0 +1,19 @@
+import { OnInit } from "@angular/core";
+import { CourseInfoService } from "./courseInfo.service";
+import { ActivatedRoute } from "@angular/router";
+import { ORLPService } from "../../orlp.service";
+export declare class CourseInfoComponent implements OnInit {
+    private route;
+    private orlp;
+    private courseService;
+    private url;
+    private decks;
+    private course;
+    private sub;
+    private errorMessage;
+    constructor(route: ActivatedRoute, orlp: ORLPService, courseService: CourseInfoService);
+    ngOnInit(): void;
+    takeCourse(): void;
+    private takeDecks(course);
+    private decodeLink();
+}
