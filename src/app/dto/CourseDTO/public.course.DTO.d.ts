9ca2a77f339bc1b22d383813475ec40375d0d5e0
zadorovskyi <zadorovskyi@gmail.com>
2017-08-07 17:17:47 +0300

    rebased to cli

--- src/app/dto/CourseDTO/public.course.DTO.d.ts
+++
@@ -1,8 +0,0 @@
-import { Link } from "../link";
-export declare class CoursePublic {
-    name: string;
-    description: string;
-    imagebase64: string;
-    self: Link;
-    constructor(name: string, description: string, imagebase64: string, self: Link);
-}
--- src/app/dto/DeckDTO/public.deck.DTO.d.ts
+++
@@ -1,8 +0,0 @@
-import { Link } from "../link";
-export declare class DeckPublic {
-    name: string;
-    description: string;
-    rating: number;
-    self: Link;
-    constructor(name: string, description: string, rating: number, self: Link);
-}
