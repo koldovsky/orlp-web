cad1308f226e26f116b6bf3104f4d539fa4755c4
YevhenPalamarchuk <yevhen.palamarchuk@gmail.com>
2017-08-15 23:07:27 +0300

    Orlp 114 - Update links in the web-server to Image-Repository in the API-Server (#24)
    * Update links in the web-server to Image-Repository in the API-Server
    
    * Update links in the web-server to Image-Repository in the API-Server admin

--- src/app/dto/CategoryDTO/link.category.DTO.ts
+++ src/app/dto/CategoryDTO/link.category.DTO.ts
@@ -3,16 +3,16 @@ import {Link} from '../link';
 export class CategoryLink {
   public name: string;
   public description: string;
-  public imagebase64: string;
+  public image: string;
   public self: Link;
   public decks: Link;
   public courses: Link;
 
 
-  constructor(name: string, description: string, imagebase64: string, self: Link, decks: Link, courses: Link) {
+  constructor(name: string, description: string, image: string, self: Link, decks: Link, courses: Link) {
     this.name = name;
     this.description = description;
-    this.imagebase64 = imagebase64;
+    this.image = image;
     this.self = self;
     this.decks = decks;
     this.courses = courses;
--- src/app/dto/dto.converter.ts
+++ src/app/dto/dto.converter.ts
@@ -34,14 +34,14 @@ export class DTOConverter {
     let self: Link = DTOConverter.jsonToLink('self', data._links.self);
     let decks: Link = DTOConverter.jsonToLink('decks', data._links.decks);
 
-    return new CourseLink(data.name, data.description, data.imagebase64, self, decks, data.rating);
+    return new CourseLink(data.name, data.description, data.image, self, decks, data.rating);
   }
 
   public static jsonToPublicLinkCourseWithId(data: any): CourseLinkWithId {
     let self: Link = DTOConverter.jsonToLink('self', data._links.self);
     let decks: Link = DTOConverter.jsonToLink('decks', data._links.decks);
 
-    return new CourseLinkWithId(data.courseId, data.name, data.description, data.rating, data.imagebase64, self, decks);
+    return new CourseLinkWithId(data.courseId, data.name, data.description, data.rating, data.image, self, decks);
   }
 
     public static jsonToDeckLinkByFolder(data: any): DeckLinkByFolder {
@@ -54,7 +54,7 @@ export class DTOConverter {
     public static jsonToPublicCourse(data: any): CoursePublic {
         let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CoursePublic(data.name, data.description, data.imagebase64, self);
+        return new CoursePublic(data.name, data.description, data.image, self);
     }
 
     public static jsonToPublicDeck(data: any): DeckPublic {
@@ -73,19 +73,19 @@ export class DTOConverter {
     public static jsonToTopCategory(data: any): CategoryTop {
         let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CategoryTop(data.name, data.imagebase64, self);
+        return new CategoryTop(data.name, data.image, self);
     }
 
     public static jsonToTopCourse(data: any): CourseTop {
         let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CourseTop(data.name, data.imagebase64, self);
+        return new CourseTop(data.name, data.image, self);
     }
 
     public static jsonToPublicCategories(data: any): CategoriesPublic {
         let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CategoriesPublic(data.name, data.description, data.imagebase64, self);
+        return new CategoriesPublic(data.name, data.description, data.image, self);
     }
 
     public static jsonToPublicCategory(data: any): CategoryLink {
@@ -93,7 +93,7 @@ export class DTOConverter {
         let courses: Link = DTOConverter.jsonToLink('courses', data._links.courses);
         let decks: Link = DTOConverter.jsonToLink('decks', data._links.decks);
 
-        return new CategoryLink(data.name, data.description, data.imagebase64, self, decks, courses);
+        return new CategoryLink(data.name, data.description, data.image, self, decks, courses);
     }
 
     public static jsonToAdminUsers(data: any): AdminUsers {
