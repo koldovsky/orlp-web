df981dae4c384c549ec30d5b4ea85e02ca3bf985
zadorovskyi <zadorovskyi@gmail.com>
2017-08-16 12:17:49 +0300

    Changed imageBase64 to image

--- src/app/dto/CourseDTO/linkWithId.course.DTO.ts
+++ src/app/dto/CourseDTO/linkWithId.course.DTO.ts
@@ -4,14 +4,13 @@ export class CourseLinkWithId {
   public courseId: number;
   public name: string;
   public description: string;
-  public rating: number;
   public image: string;
   public self: Link;
   public decks: Link;
   public isUserOwnCourse: boolean;
   public rating: number;
 
-  constructor( courseId: number, name: string, description: string, rating: number, imagebase64: string, self: Link, decks: Link) {
+  constructor( courseId: number, name: string, description: string, rating: number, image: string, self: Link, decks: Link) {
     this.name = name;
     this.description = description;
     this.rating = rating;
