c27947ce640f148c089384248994c0ed4395a566
LeOrK2312 <leorkestra@gmail.com>
2017-08-17 12:37:11 +0300

    added view for audit

--- src/app/dto/dto.converter.ts
+++ src/app/dto/dto.converter.ts
@@ -13,14 +13,15 @@ import {CourseLink} from './CourseDTO/link.course.DTO';
 import {UserDetailsDto} from './UserDetailsDto';
 import {DeckLinkByFolder} from './DeckDTO/linkByFolder.deck.DTO';
 import {CourseLinkWithId} from './CourseDTO/linkWithId.course.DTO';
+import {AdminAudit} from './AdminDTO/admin.audit.DTO';
 
 export class DTOConverter {
 
-    public static jsonToPublicCards(data: any): CardPublic {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+  public static jsonToPublicCards(data: any): CardPublic {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CardPublic(data.answer, data.question, self);
-    }
+    return new CardPublic(data.answer, data.question, self);
+  }
 
   public static jsonToUserDTO(data: any): UsersDTO {
     const self: Link = DTOConverter.jsonToLink('self', data._links.self);
@@ -44,77 +45,82 @@ export class DTOConverter {
     return new CourseLinkWithId(data.courseId, data.name, data.description, data.rating, data.image, self, decks);
   }
 
-    public static jsonToDeckLinkByFolder(data: any): DeckLinkByFolder {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
-        let cards: Link = DTOConverter.jsonToLink('cards', data._links.cards);
+  public static jsonToDeckLinkByFolder(data: any): DeckLinkByFolder {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+    let cards: Link = DTOConverter.jsonToLink('cards', data._links.cards);
 
-        return new DeckLinkByFolder(data.name, data.description, self, cards);
-    }
+    return new DeckLinkByFolder(data.name, data.description, self, cards);
+  }
 
-    public static jsonToPublicCourse(data: any): CoursePublic {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+  public static jsonToPublicCourse(data: any): CoursePublic {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CoursePublic(data.name, data.description, data.image, self);
-    }
+    return new CoursePublic(data.name, data.description, data.image, self);
+  }
 
-    public static jsonToPublicDeck(data: any): DeckPublic {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+  public static jsonToPublicDeck(data: any): DeckPublic {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new DeckPublic(data.name, data.description, data.rating, self);
-    }
+    return new DeckPublic(data.name, data.description, data.rating, self);
+  }
 
-    public static jsonToDeckLinkByCategory(data: any): DeckLinkByCategory {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
-        let cards: Link = DTOConverter.jsonToLink('cards', data._links.cards);
+  public static jsonToDeckLinkByCategory(data: any): DeckLinkByCategory {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+    let cards: Link = DTOConverter.jsonToLink('cards', data._links.cards);
 
-        return new DeckLinkByCategory(data.name, data.description, self, cards, data.deckId);
-    }
+    return new DeckLinkByCategory(data.name, data.description, self, cards, data.deckId);
+  }
 
-    public static jsonToTopCategory(data: any): CategoryTop {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+  public static jsonToTopCategory(data: any): CategoryTop {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CategoryTop(data.name, data.image, self);
-    }
+    return new CategoryTop(data.name, data.image, self);
+  }
 
-    public static jsonToTopCourse(data: any): CourseTop {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+  public static jsonToTopCourse(data: any): CourseTop {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CourseTop(data.name, data.image, self);
-    }
+    return new CourseTop(data.name, data.image, self);
+  }
 
-    public static jsonToPublicCategories(data: any): CategoriesPublic {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+  public static jsonToPublicCategories(data: any): CategoriesPublic {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
 
-        return new CategoriesPublic(data.name, data.description, data.image, self);
-    }
+    return new CategoriesPublic(data.name, data.description, data.image, self);
+  }
 
-    public static jsonToPublicCategory(data: any): CategoryLink {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
-        let courses: Link = DTOConverter.jsonToLink('courses', data._links.courses);
-        let decks: Link = DTOConverter.jsonToLink('decks', data._links.decks);
+  public static jsonToPublicCategory(data: any): CategoryLink {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+    let courses: Link = DTOConverter.jsonToLink('courses', data._links.courses);
+    let decks: Link = DTOConverter.jsonToLink('decks', data._links.decks);
 
-        return new CategoryLink(data.name, data.description, data.image, self, decks, courses);
-    }
+    return new CategoryLink(data.name, data.description, data.image, self, decks, courses);
+  }
 
-    public static jsonToAdminUsers(data: any): AdminUsers {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
-        return new AdminUsers(data.firstName, data.lastName, data.email, data.accountStatus, self);
-    }
+  public static jsonToAdminUsers(data: any): AdminUsers {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+    return new AdminUsers(data.firstName, data.lastName, data.email, data.accountStatus, self);
+  }
 
-    public static jsonToUserDetails(data: any): UserDetailsDto {
-        let self: Link = DTOConverter.jsonToLink('self', data._links.self);
-        return new UserDetailsDto(data.firstName, data.lastName, data.email, data.image, data.authorities, self);
-    }
+  public static jsonToAdminAudit(data: any): AdminAudit {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+    return new AdminAudit(data.accountEmail, data.action, data.ipAddress, data.role, data.time, self);
+  }
 
-    public static jsonToLink(rel: string, data: any): Link {
-        return new Link(rel, data.href);
-    }
+  public static jsonToUserDetails(data: any): UserDetailsDto {
+    let self: Link = DTOConverter.jsonToLink('self', data._links.self);
+    return new UserDetailsDto(data.firstName, data.lastName, data.email, data.image, data.authorities, self);
+  }
 
-    public static jsonArrayToCollection(callback: Function, data: Array<any>): Array<any> {
-        let array: Array<any> = [];
-        data.forEach(element => {
-            array.push(callback(element));
-        });
-        return array;
-    }
+  public static jsonToLink(rel: string, data: any): Link {
+    return new Link(rel, data.href);
+  }
+
+  public static jsonArrayToCollection(callback: Function, data: Array<any>): Array<any> {
+    let array: Array<any> = [];
+    data.forEach(element => {
+      array.push(callback(element));
+    });
+    return array;
+  }
 }
---
+++ src/app/page/admin/audit/admin.audit.component.ts
@@ -0,0 +1,121 @@
+import {Component, OnInit} from '@angular/core';
+import {AdminAudit} from '../../../dto/AdminDTO/admin.audit.DTO';
+import {AdminAuditService} from './admin.audit.service';
+
+@Component({
+  providers: [AdminAuditService],
+  templateUrl: ('./admin.audit.component.html'),
+  styleUrls: ['./admin.audit.style.css']
+})
+
+export class AdminAuditComponent implements OnInit {
+  auditList: AdminAudit[];
+  emailAsc = true;
+  actionAsc = true;
+  ipAsc = true;
+  roleAsc = true;
+  timeAsc = true;
+
+  constructor(private adminAuditService: AdminAuditService) {
+  }
+
+  ngOnInit(): void {
+    this.adminAuditService.getFullAuditList()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public toggleEmail(): void {
+    this.emailAsc = !this.emailAsc;
+    if (this.emailAsc) {
+      this.getAuditListSortedByAccountEmailDesc();
+    } else {
+      this.getAuditListSortedByAccountEmailAsc();
+    }
+  }
+
+  public toggleAction(): void {
+    this.actionAsc = !this.actionAsc;
+    if (this.actionAsc) {
+      this.getAuditListSortedByActionDesc();
+    } else {
+      this.getAuditListSortedByActionAsc();
+    }
+  }
+
+  public toggleIp(): void {
+    this.ipAsc = !this.ipAsc;
+    if (this.ipAsc) {
+      this.getAuditListSortedByIpAddressDesc();
+    } else {
+      this.getAuditListSortedByIpAddressAsc();
+    }
+  }
+
+  public toggleRole(): void {
+    this.roleAsc = !this.roleAsc;
+    if (this.roleAsc) {
+      this.getAuditListSortedByRoleDesc();
+    } else {
+      this.getAuditListSortedByRoleAsc();
+    }
+  }
+
+  public toggleTime(): void {
+    this.timeAsc = !this.timeAsc;
+    if (this.timeAsc) {
+      this.getAuditListSortedByTimeDesc();
+    } else {
+      this.getAuditListSortedByTimeAsc();
+    }
+  }
+
+  public getAuditListSortedByAccountEmailAsc(): void {
+    this.adminAuditService.getAuditListSortedByAccountEmailAsc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByAccountEmailDesc(): void {
+    this.adminAuditService.getAuditListSortedByAccountEmailDesc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByActionAsc(): void {
+    this.adminAuditService.getAuditListSortedByActionAsc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByActionDesc(): void {
+    this.adminAuditService.getAuditListSortedByActionDesc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByIpAddressAsc(): void {
+    this.adminAuditService.getAuditListSortedByIpAddressAsc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByIpAddressDesc(): void {
+    this.adminAuditService.getAuditListSortedByIpAddressDesc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByTimeAsc(): void {
+    this.adminAuditService.getAuditListSortedByTimeAsc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByTimeDesc(): void {
+    this.adminAuditService.getAuditListSortedByTimeDesc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByRoleAsc(): void {
+    this.adminAuditService.getAuditListSortedByRoleAsc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+
+  public getAuditListSortedByRoleDesc(): void {
+    this.adminAuditService.getAuditListSortedByRoleDesc()
+      .subscribe(auditList => this.auditList = auditList);
+  }
+}
