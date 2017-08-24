54f24457d76806704e59c01de36539f212870b2c
LeOrK2312 <leorkestra@gmail.com>
2017-08-24 15:43:49 +0300

    ORLP-128 Fixed click on user photo, should redirect to cabinet ORLP-129 Renamed default folder to my folder on UI ORLP-131 Fixed sign out, should disappear when user click on a page ORLP-133 Web: Deny access to the admin part to other users

--- src/app/page/admin/admin.main.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    templateUrl: ('./admin.main.component.html'),
-    styleUrls: ['./admin.style.css']
-})
-export class AdminMainComponent {}
