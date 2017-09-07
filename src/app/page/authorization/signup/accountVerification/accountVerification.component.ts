0a4842f0f668434e2099dd8211b46a8e9d10ed49
zadorovskyi <zadorovskyi@gmail.com>
2017-09-07 15:00:39 +0300

    Refactored (#38)
    refactore

--- src/app/page/authorization/signup/accountVerification/accountVerification.component.ts
+++ src/app/page/authorization/signup/accountVerification/accountVerification.component.ts
@@ -5,7 +5,7 @@ import {AccountVerificationService} from './accountVerification.service';
 
 
 @Component({
-  template: `q`,
+  template: `<div></div>`,
 })
 export class AccountVerificationComponent implements OnInit {
   token: string;
