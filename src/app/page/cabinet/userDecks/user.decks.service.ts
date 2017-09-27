b32a93057c94727503d2b1d57c5d2683408d5248
b0hdan <bohdan.dub@gmail.com>
2017-09-27 22:27:02 +0300

    Links have been changed.

--- src/app/page/cabinet/userDecks/user.decks.service.ts
+++ src/app/page/cabinet/userDecks/user.decks.service.ts
@@ -25,7 +25,7 @@ export class UserDecksService {
   }
 
   getOnlyDecksCreatedByTheUser(userId: number): Observable<DeckDTO[]> {
-    return this.orlp.get('api/private/user/folder/' + userId + '/decks/own').map((response: Response) =>
+    return this.orlp.get('api/private/user/folder/decks/own').map((response: Response) =>
       <DeckDTO[]> DTOConverter.jsonArrayToCollection(DTOConverter.jsonToDeck, response.json()));
   }
 
@@ -35,12 +35,12 @@ export class UserDecksService {
   }
 
   editDeck(deck: DeckDTO) {
-    return this.orlp.put('api/private/category/' + deck.categoryId + '/deck/' + deck.deckId, deck)
+    return this.orlp.put('api/private/deck/' + deck.deckId, deck)
       .map((response: Response) => response.json());
   }
 
   deleteDeck(categoryId: number, deckId: number) {
-    return this.orlp.delete('api/private/category/' + categoryId + '/deck/' + deckId)
+    return this.orlp.delete('api/private/deck/' + deckId)
       .map((response: Response) => response.json());
   }
 }
