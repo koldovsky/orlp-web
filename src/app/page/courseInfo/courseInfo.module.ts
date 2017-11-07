8a76579dd23fd29f1293a59ba7c9cfedff45ebdf
IhorF <Ihor_F@i.ua>
2017-11-07 22:59:56 +0200

    ORLP-238.UI for CourseComment

--- src/app/page/courseInfo/courseInfo.module.ts
+++ src/app/page/courseInfo/courseInfo.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
-import {CommonModule} from "@angular/common";
-import {RouterModule} from "@angular/router";
-import {StarRatingModule} from "angular-star-rating";
-import {CourseInfoService} from "./courseInfo.service";
-import {CourseInfoComponent} from "./courseInfo.component";
-import {CommentComponent} from "../comments/comment.component";
-import {CommentService} from "../comments/comment.service";
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {StarRatingModule} from 'angular-star-rating';
+import {CourseInfoService} from './courseInfo.service';
+import {CourseInfoComponent} from './courseInfo.component';
+import {CommentComponent} from '../comments/comment.component';
+import {CommentService} from '../comments/comment.service';
 
 @NgModule({
     imports: [
