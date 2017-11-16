37feb312dec5634ce7421814e438d72a7f3a25e5
SolukAndrian <andriansoluk@gmail.com>
2017-11-16 17:33:00 +0200

    add sorting and pagination for categories

--- src/app/page/user/categories/user.categories.component.ts
+++ src/app/page/user/categories/user.categories.component.ts
@@ -3,6 +3,7 @@ import {CategoriesPublic} from '../../../dto/CategoryDTO/public.categories';
 import {UserCategoriesService} from './user.categories.service';
 import {ORLPService} from '../../../services/orlp.service';
 import {Link} from '../../../dto/link';
+import {TableColumnDTO} from "../../../dto/TableColumnDTO";
 
 @Component({
   selector: 'app-user-all-categories',
@@ -15,17 +16,45 @@ export class UserCategoriesComponent implements OnInit {
   categories: CategoriesPublic[];
   errorMessage: string;
   listFilter: string;
+  actionSort = true;
+  currentPage = 1;
+  lastPage: number;
+  categoryColumns: TableColumnDTO[] = [new TableColumnDTO('id', '#', '\u2191'), new TableColumnDTO('name', 'Category Name', '')
+    , new TableColumnDTO('description', 'Category Description', '')];
+  selectedSortedParam: TableColumnDTO = this.categoryColumns[0];
 
   constructor(private userCategoriesService: UserCategoriesService,
               private orlpService: ORLPService) {
   }
 
   ngOnInit(): void {
-    this.userCategoriesService.getCategories()
-      .subscribe(categories => this.categories = categories,
-        error => this.errorMessage = <any>error);
+    this.getCategoriesByPage(this.currentPage);
   }
 
+  public getCategoriesByPage(numberPage: number)  {
+    this.userCategoriesService.getCategoriesByPage(numberPage, this.selectedSortedParam.nameColumnParam, this.actionSort)
+      .subscribe(category => {
+        this.currentPage = numberPage;
+        this.lastPage = category.totalPages;
+        this.categories = category.categories;
+      });
+  }
+  public sortBy(param: TableColumnDTO) {
+    if (param === this.selectedSortedParam) {
+      this.actionSort = !this.actionSort;
+    }else {
+      this.actionSort = true;
+      this.selectedSortedParam.symbolSorting = '';
+    }
+    if (this.actionSort) {
+      param.symbolSorting = '\u2191';
+    } else {
+      param.symbolSorting = '\u2193';
+    }
+
+    this.selectedSortedParam = param;
+    this.getCategoriesByPage(this.currentPage);
+  }
   getCategoryLink(link: Link): string {
     return this.orlpService.getShortLink(link);
   }
--- src/app/page/user/categories/user.categories.service.ts
+++ src/app/page/user/categories/user.categories.service.ts
@@ -5,6 +5,7 @@ import {Response} from '@angular/http';
 import {CategoriesPublic} from '../../../dto/CategoryDTO/public.categories';
 import {DTOConverter} from '../../../dto/dto.converter';
 import {NGXLogger} from 'ngx-logger';
+import {CategoriesByPageDTO} from "../../../dto/CategoryDTO/link.categories.by.page.DTO";
 
 @Injectable()
 export class UserCategoriesService {
@@ -17,6 +18,13 @@ export class UserCategoriesService {
     return this.orlp.get('api/category')
       .map((response: Response) => <CategoriesPublic[]> DTOConverter
         .jsonArrayToCollection(DTOConverter.jsonToPublicCategories, response.json()))
+      .catch(this.handleError);
+  }
+
+  getCategoriesByPage(numberPage: number, selectedSortingParam: string, ascending: boolean): Observable<CategoriesByPageDTO> {
+    return this.orlp.get('api/sortedCategoriesByPage/top?p=' + numberPage + '&sortBy=' + selectedSortingParam + '&asc=' + ascending)
+      .map((response: Response) => <CategoriesByPageDTO> DTOConverter
+        .jsonToCategoriesByPage(response.json()))
       .catch(this.handleError);
   }
 
