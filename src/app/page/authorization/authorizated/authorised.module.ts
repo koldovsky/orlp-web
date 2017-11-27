bb124a086e164869159e80cd4f418f455fa853cf
SolukAndrian <andriansoluk@gmail.com>
2017-11-27 21:09:19 +0200

    Fix remove never used lines

--- src/app/page/authorization/authorizated/authorised.module.ts
+++ src/app/page/authorization/authorizated/authorised.module.ts
@@ -1,14 +1,7 @@
 import {NgModule} from '@angular/core';
-import {HttpModule} from '@angular/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {AuthorisedComponent} from './authorised.component';
-import {Angular2SocialLoginModule} from 'angular2-social-login';
-import {ReCaptchaModule} from 'angular2-recaptcha';
-import {AuthorizationService} from '../authorization.service';
-import {environment} from '../../../../environments/environment';
-import {AuthorizationUserGuardService} from "../authorization.user.guard.service";
+import {AuthorizationUserGuardService} from '../authorization.user.guard.service';
 
 @NgModule({
     imports: [
@@ -25,4 +18,3 @@ import {AuthorizationUserGuardService} from "../authorization.user.guard.service
 export class AuthorisedModule {
     constructor() { }
 }
-Angular2SocialLoginModule.loadProvidersScripts(environment.providers);
