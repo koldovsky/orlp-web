65116d361b9ae689b5fa751b04b9c04f1eceb705
Maks-Chornyi <380960310411@mail.ua>
2018-10-29 15:36:48 +0200

    deleted frmo tracking by git package-lock.json

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+import {NgxLoggerLevel} from 'ngx-logger';
+
+export const environment = {
+  production: false,
+  logger: {
+    level: NgxLoggerLevel.DEBUG
+  },
+  providers: {
+    google: {
+      clientId: '109414995516-fftdn22fnpfdh80fgfsvpq1ht97hdkl5.apps.googleusercontent.com'
+    },
+    facebook: {
+      clientId: '439882899718340',
+      apiVersion: 'v2.9'
+    }
+  },
+  SERVER_ADDRESS: 'http://localhost:8080/',
+  SITE_KEY: '6Lds0nEUAAAAACnNAVV5hp1Ubt1FfnZBScLEIxCa'
+};
